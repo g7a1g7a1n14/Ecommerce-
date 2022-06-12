bf36e4825f6cb52135478031b03a4689d28f321f
g7a1g7a1n14 <100964115+g7a1g7a1n14@users.noreply.github.com>
2022-06-12 10:40:43 +0530

    Update About.js

--- frontend/src/component/layout/About/About.js
+++ frontend/src/component/layout/About/About.js
@@ -18,7 +18,7 @@ const About = () => {
           <div>
             <Avatar
               style={{ width: "10vmax", height: "10vmax", margin: "2vmax 0" }}
-              src="https://res.cloudinary.com/tripleayt/image/upload/v1631555947/products/jpyibarlaxawvcvqjv5b.png"
+              src="IMG_20211031_112434.jpg"
               alt="Founder"
             />
             <Typography>Gagan Chaturvedi</Typography>
